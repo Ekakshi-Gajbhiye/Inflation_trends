90380ef2aee4f0bbb0732a3883a2edb9d6626470
Ekakshi Gajbhiye <gekakshi@gmail.com>
2025-03-25 16:22:51 +0530

    made changes in fetchung

--- fetch.py
+++ fetch.py
@@ -9,18 +9,14 @@ FRED_API_KEY = st.secrets["fred_api"]["api_key"]
 if not FRED_API_KEY:
     raise ValueError("API Key not found! Set FRED_API_KEY in environment variables.")
 
-# URL for CPI (Consumer Price Index)
 url = f"https://api.stlouisfed.org/fred/series/observations?series_id=CPIAUCSL&api_key={FRED_API_KEY}&file_type=json"
 
-# Fetch data
 response = requests.get(url)
 data = response.json()
 
-# Convert to DataFrame
 inflation_data = pd.DataFrame(data["observations"])
 inflation_data["value"] = inflation_data["value"].astype(float)
 
-# Save locally
 inflation_data.to_csv("inflation_data.csv", index=False)
 print("Data saved successfully!")
 #print(json.dumps(data, indent=4))
